864b51f6c81c90911b2163aa81fb145b19bdf544
Mayra Landa <mlandae16@gmail.com>
2024-09-10 11:20:19 -0700

    made with express

--- index.js
+++ index.js
@@ -1,38 +1,31 @@
-const http = require('http');
-const fs = require('fs');
+require('dotenv').config();
+
+const exp = require('constants');
+const express = require('express');
 const path = require('path');
 
-const server = http.createServer((req, res) => {
-  let filePath = '';
+const app = express();
 
-  switch (req.url){
-    case '/':
+const port = process.env.PORT || 8080;
 
-      filePath = 'index.html';
-      break;
-    case '/about':
-      filePath = 'about.html';
-      break;
-    case '/contact-me':
-      filePath = 'contact-me.html';
-      break;
-    default: 
-      filePath = '404.html';
-      break;
-  }
+app.use(express.static(path.join(__dirname)));
 
-  fs.readFile(path.join(__dirname, filePath), (err, data) => {
-    if (err) {
-      res.writeHead(500, {'Content-Type': 'text/plain'});
-      res.end('Server Error');
-    } else{
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.end(data);
-    }
-  })
-})
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-const PORT = 8080;
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-})
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "about.html"));
+});
+
+app.get("/contact-me", (req, res) => {
+  res.sendFile(path.join(__dirname, "contact-me.html"));
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "404.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
